0ae2362d46120db33e0945179f387a31a3c7821c
Emile Mathieu <emilepc2003@hotmail.com>
2024-05-29 21:48:07 +0100

    First commit for the recap

---
+++ src/main/java/com/example/springbootrecap/SpringBootRecapApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootrecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRecapApplication.class, args);
+    }
+
+}
