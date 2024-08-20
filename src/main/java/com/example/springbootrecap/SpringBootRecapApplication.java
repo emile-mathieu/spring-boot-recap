ee989b31a84ec29cf44cdc746295a1ed1d4fe968
Emile Mathieu <emilepc2003@hotmail.com>
2024-08-20 12:34:42 +0700

    Added H2 mem Database

--- src/main/java/com/example/springbootrecap/SpringBootRecapApplication.java
+++ src/main/java/com/example/springbootrecap/SpringBootRecapApplication.java
@@ -1,13 +1,30 @@
 package com.example.springbootrecap;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class SpringBootRecapApplication {
+@Log
+public class SpringBootRecapApplication implements CommandLineRunner {
 
+    private final DataSource datasource;
+
+    public SpringBootRecapApplication(final DataSource dataSource ) {
+        this.datasource = dataSource;
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRecapApplication.class, args);
     }
 
+    @Override
+    public void run(final String...args){
+        log.info("DataSource : " + datasource.toString());
+        final JdbcTemplate restTemplate = new JdbcTemplate(datasource);
+        restTemplate.execute("select 1");
+    }
 }
